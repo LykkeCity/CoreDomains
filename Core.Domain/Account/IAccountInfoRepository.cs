5d4b1c8091cb575746f831672ec9b0b5dcd42d25
Julia Tolipova <tuxina@yandex.ru>
2016-08-26 13:28:03 +0100

    added repository and model for account entity

---
+++ Core.Domain/Account/IAccountInfoRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Lykke.Core.Domain.Account.Models;
+
+namespace Lykke.Core.Domain.Account
+{
+    public interface IAccountInfoRepository
+    {
+        Task<AccountInfo> GetAsync(string accountId);
+
+        Task UpdateAsync(AccountInfo accountInfo);
+
+        Task AddAsync(AccountInfo accountInfo);
+
+        Task<IEnumerable<AccountInfo>> GetAllAsync();
+    }
+}
---
+++ Core.Domain/Account/Models/AccountInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lykke.Core.Domain.Account.Models
+{
+    public class AccountInfo
+    {
+        public string AccountId { get; set; }
+        public double Balance { get; set; }
+        public string BaseAssetId { get; set; }
+        public double Shoulder { get; set; }
+    }
+}
