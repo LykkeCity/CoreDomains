245073426e20f86b24045bbcc3c10edaf1693431
Julia Tolipova <tuxina@yandex.ru>
2016-08-26 13:29:26 +0100

    added asset pair quote repository

---
+++ Core.Domain/Assets/IAssetPairQuoteRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Lykke.Core.Domain.Assets.Models;
+
+namespace Lykke.Core.Domain.Assets
+{
+    public interface IAssetPairQuoteRepository
+    {
+        Task<IEnumerable<AssetPairQuote>> GetAllAsync();
+
+        Task<AssetPairQuote> GetAsync(string assertPairId);
+
+        Task AddAllAsync(IEnumerable<AssetPair> assetPairs);
+
+        Task<AssetPairQuote> UpdateAsync(AssetPair assetPair);
+    }
+}
