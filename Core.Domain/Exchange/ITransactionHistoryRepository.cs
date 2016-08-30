534a09a5769f432d208c89e217d1a34f014b9553
Julia Tolipova <tuxina@yandex.ru>
2016-08-30 14:38:14 +0100

    added repository and model for transaction history entity

---
+++ Core.Domain/Exchange/ITransactionHistoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Lykke.Core.Domain.Assets.Models;
+using Lykke.Core.Domain.Exchange.Models;
+
+namespace Lykke.Core.Domain.Exchange
+{
+    public interface ITransactionHistoryRepository
+    {
+        Task AddAsync(OrderInfo orderInfo, AssetPairQuote currentQuote);
+
+        Task<IEnumerable<TransactionHistory>> GetAllAsync(string accountId);
+    }
+}
---
+++ Core.Domain/Exchange/Models/TransactionHistory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lykke.Core.Domain.Exchange.Models
+{
+    public class TransactionHistory
+    {
+        public string AccountId { get; set; }
+        public string TransactionId { get; set; }
+        public string AssetPairId { get; set; }
+        public DateTime CompletedAt { get; set; }
+        public double ProfitLoss { get; set; }
+        public double Price { get; set; }
+    }
+}
