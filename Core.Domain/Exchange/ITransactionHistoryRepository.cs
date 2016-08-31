c9940bce7afaaf97f1eaa48589e15e98b172dc62
Julia Tolipova <tuxina@yandex.ru>
2016-08-31 15:29:59 +0100

    updated TransactionHistoryRepo method arguments

--- Core.Domain/Exchange/ITransactionHistoryRepository.cs
+++ Core.Domain/Exchange/ITransactionHistoryRepository.cs
@@ -1,13 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using Lykke.Core.Domain.Assets.Models;
 using Lykke.Core.Domain.Exchange.Models;
 
 namespace Lykke.Core.Domain.Exchange
 {
     public interface ITransactionHistoryRepository
     {
-        Task AddAsync(OrderInfo orderInfo, AssetPairQuote currentQuote);
+        Task AddAsync(TransactionHistory transactionHistory);
 
         Task<IEnumerable<TransactionHistory>> GetAllAsync(string accountId);
     }
