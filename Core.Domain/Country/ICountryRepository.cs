af44281d9508a7827472d02f9de9545d094b584f
Julia Tolipova <tuxina@yandex.ru>
2016-09-08 14:59:17 +0100

    added repository and model for country entity

---
+++ Core.Domain/Country/ICountryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Lykke.Core.Domain.Country.Models;
+
+namespace Lykke.Core.Domain.Country
+{
+    public interface ICountryRepository
+    {
+        Task<IEnumerable<CountryItem>> GetAllAsync(string language);
+
+        Task<CountryItem> GetAsync(string language, string iso3);
+    }
+}
---
+++ Core.Domain/Country/Models/CountryItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Lykke.Core.Domain.Country.Models
+{
+    public class CountryItem
+    {
+        public string Iso3 { get; set; }
+        public string Iso2 { get; set; }
+        public string Name { get; set; }
+        public string Prefix { get; set; }
+    }
+}
